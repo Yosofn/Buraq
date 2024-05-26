b21a6058ede6d57fdafd4957085c51add3c6afd0
Yosofn <yousef>
2024-05-26 23:30:14 +0300

    Add project files.

---
+++ Controllers/Products.cs
@@ -0,0 +1,40 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buraq.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+
+            var products = await _context.Products.ToArrayAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+
+            return await _context.Products.FindAsync(id);
+
+        }
+    }
+
+
+
+
+}
